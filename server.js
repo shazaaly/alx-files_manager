fa8d66ca2db5d5d5d9b5797fc5b66cdbef1081fc
shazaaly <shaza.aly@gmail.com>
2024-03-10 00:43:22 +0200

    Refactor server.js to use ES6 import syntax and update route handling

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require('express');
+import express from 'express';
+import router from './routes/index';
+
+const port = parseInt(process.env.PORT, 10) || 5000;
 
 const app = express();
-const routes = require('./routes/index');
 
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use('/', router);
 
-app.use('/', routes);
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
 });
 
-module.exports = app;
+export default app;
