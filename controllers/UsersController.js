2f9ae252f77ac6523d828e1ba289eb89ea539339
shazaaly <shaza.aly@gmail.com>
2024-03-09 23:40:12 +0200

    Fix UserController.js: Fix typos and improve error handling

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -7,14 +7,10 @@ class UsersController {
         const {email, password} = req.body
 
         const existingUser = await dbClient.db.collection('users').findOne({ email })
-        if (existingUser) {
-            return res.status(400).json({
-                error: 'Already exist'
-            })
+        if (existingUser) return res.status(400).json({ error: 'Already exist' });
 
-        }
-        if (!email) return res.json.status(400).send({ error: 'Missing email' })
-        if (!password) return res.json.status(400).send({ error: 'Missing password' })
+        if (!email) return res.status(400).send({ error: 'Missing email' })
+        if (!password) return res.status(400).send({ error: 'Missing password' })
 
         const hashed = crypto.createHash('SHA1').update(password).digest('hex')
         const newUser = {
@@ -22,15 +18,15 @@ class UsersController {
             password: hashed
         }
         try {
-            await dbClient.db.Collection('users').insertOne(newUser)
+            await dbClient.db.collection('users').insertOne(newUser)
             return res.stataus(201).json({
                 email,
-                id : insertedId
+                id : res.insertedId
             })
 
 
         } catch (error) {
-            return res.stataus(500).json({ error: 'Error creating user' })
+            return res.status(500).json({ error: 'Error creating user' })
 
         }
 
