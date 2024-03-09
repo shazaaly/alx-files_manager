1b8a3d1c379f0f2233abb0d6d323875349d21973
shazaaly <shaza.aly@gmail.com>
2024-03-10 00:04:07 +0200

    Refactor UsersController to use ES6 imports and add validation for existing users

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,36 +1,40 @@
-const dbClient = require('../utils/db');
-const sha1 = require('sha1');
+import sha1 from 'sha1';
+import { ObjectID } from 'mongodb';
+import dbClient from '../utils/db';
+
 
 class UsersController {
-    static postNew(request, response) {
-      const { email } = request.body;
-      const { password } = request.body;
-  
-      if (!email) {
-        response.status(400).json({ error: 'Missing email' });
-        return;
+  static postNew(request, response) {
+    const { email } = request.body;
+    const { password } = request.body;
+
+    if (!email) {
+      response.status(400).json({ error: 'Missing email' });
+      return;
+    }
+    if (!password) {
+      response.status(400).json({ error: 'Missing password' });
+      return;
+    }
+
+    const users = dbClient.db.collection('users');
+    users.findOne({ email }, (err, user) => {
+      if (user) {
+        response.status(400).json({ error: 'Already exist' });
+      } else {
+        const hashedPassword = sha1(password);
+        users.insertOne(
+          {
+            email,
+            password: hashedPassword,
+          },
+        ).then((result) => {
+          response.status(201).json({ id: result.insertedId, email });
+        }).catch((error) => console.log(error));
       }
-      if (!password) {
-        response.status(400).json({ error: 'Missing password' });
-        return;
-      }
-  
-      const users = dbClient.db.collection('users');
-      users.findOne({ email }, (err, user) => {
-        if (user) {
-          response.status(400).json({ error: 'Already exist' });
-        } else {
-          const hashedPassword = sha1(password);
-          users.insertOne(
-            {
-              email,
-              password: hashedPassword,
-            },
-          ).then((result) => {
-            response.status(201).json({ id: result.insertedId, email });
-          }).catch((error) => console.log(error));
-        }
-      });
-    }}
-  
-  module.exports = UsersController;
+    });
+  }
+
+}
+
+module.exports = UsersController;
