da1ceb5e8deb2d9c5adc11e7641c24a1f4e6a67e
shazaaly <shaza.aly@gmail.com>
2024-03-10 00:53:28 +0200

    Refactor UsersController to use ES6 imports and async/await

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,8 +1,8 @@
-const sha1 = require('sha1');
-const dbClient = require('../utils/db');
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
 
 class UsersController {
-  static postNew(request, response) {
+  static async postNew(request, response) {
     const { email, password } = request.body;
 
     if (!email) {
@@ -13,19 +13,15 @@ class UsersController {
     }
 
     const users = dbClient.db.collection('users');
-    users.findOne({ email }, async (err, user) => {
-      if (user) {
-        return response.status(400).json({ error: 'Already exist' });
-      }
-      const hashedPassword = sha1(password);
-      try {
-        const result = await users.insertOne({ email, password: hashedPassword });
-        return response.status(201).json({ id: result.insertedId, email });
-      } catch (error) {
-        return response.status(500).json({ error: 'Error creating user' });
-      }
-    });
+    const user = await users.findOne({ email });
+    if (user) {
+      return response.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = sha1(password);
+    const result = await users.insertOne({ email, password: hashedPassword });
+    return response.status(201).json({ id: result.insertedId, email });
   }
 }
 
-module.exports = UsersController;
+export default UsersController;
